08c9e76500ee9969c84813865c0baef2255c2027
Steve Hughes <yosser@gmail.com>
2023-04-13 08:51:29 +0100

    First commit, basic network code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import { NeuralNetwork } from './libs/neuralNetwork';
 import './App.css';
 
 function App() {
+
+
+
+  const inputNodes = 2;
+  const hiddenNodes = 4;
+  const outputNodes = 1;
+  const learningRate = 0.1;
+
+  const neuralNetwork = new NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate);
+
+  const trainingData = [
+    { input: [0, 0], target: [0] },
+    { input: [0, 1], target: [1] },
+    { input: [1, 0], target: [1] },
+    { input: [1, 1], target: [0] },
+  ];
+
+  // Train the neural network
+  const epochs = 1000;
+  for (let i = 0; i < epochs; i++) {
+    if (i % 1000 === 0) console.log(`Epoch: ${i}`);
+    trainingData.forEach(data => {
+      neuralNetwork.train(data.input, data.target);
+    });
+  }
+
+  // Test the neural network
+  trainingData.forEach(data => {
+    const output = neuralNetwork.predict(data.input);
+    console.log(JSON.stringify(output));
+    console.log(`Input: [${data.input}] | Target: [${data.target}] | Prediction: [${output.map(Math.round)}]`);
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello mum
     </div>
   );
 }
---
+++ src/libs/neuralNetwork.ts
@@ -0,0 +1,73 @@
+// neuralNetwork.ts
+
+export class NeuralNetwork {
+    inputNodes: number;
+    hiddenNodes: number;
+    outputNodes: number;
+    learningRate: number;
+    weightsInputHidden: number[][];
+    weightsHiddenOutput: number[][];
+
+    constructor(inputNodes: number, hiddenNodes: number, outputNodes: number, learningRate: number) {
+        this.inputNodes = inputNodes;
+        this.hiddenNodes = hiddenNodes;
+        this.outputNodes = outputNodes;
+        this.learningRate = learningRate;
+
+        this.weightsInputHidden = this.initializeWeights(inputNodes, hiddenNodes);
+        this.weightsHiddenOutput = this.initializeWeights(hiddenNodes, outputNodes);
+    }
+
+    private initializeWeights(rows: number, cols: number): number[][] {
+        return Array.from({ length: rows }, () => Array.from({ length: cols }, () => Math.random() * 2 - 1));
+    }
+
+    private sigmoid(x: number): number {
+        return 1 / (1 + Math.exp(-x));
+    }
+
+    private sigmoidDerivative(x: number): number {
+        return x * (1 - x);
+    }
+
+    public train(inputs: number[], targets: number[]): void {
+        // Forward propagation
+        const hiddenInputs = this.feedForward(inputs, this.weightsInputHidden);
+        const hiddenOutputs = hiddenInputs.map(this.sigmoid);
+        const finalInputs = this.feedForward(hiddenOutputs, this.weightsHiddenOutput);
+        const finalOutputs = finalInputs.map(this.sigmoid);
+
+        // Calculate output errors
+        const outputErrors = targets.map((t, i) => t - finalOutputs[i]);
+
+        // Backpropagate errors from output to hidden layer
+        const hiddenErrors = this.weightsHiddenOutput.map((weights, i) =>
+            outputErrors.reduce((sum, error, j) => sum + error * weights[j], 0)
+        );
+
+        // Update weights between hidden and output layers
+        this.weightsHiddenOutput = this.weightsHiddenOutput.map((weights, i) =>
+            weights.map((weight, j) => weight + this.learningRate * outputErrors[j] * finalOutputs[j] * (1 - finalOutputs[j]) * hiddenOutputs[i])
+        );
+
+        // Update weights between input and hidden layers
+        this.weightsInputHidden = this.weightsInputHidden.map((weights, i) =>
+            weights.map((weight, j) => weight + this.learningRate * hiddenErrors[j] * hiddenOutputs[j] * (1 - hiddenOutputs[j]) * inputs[i])
+        );
+    }
+
+    public predict(inputs: number[]): number[] {
+        const hiddenInputs = this.feedForward(inputs, this.weightsInputHidden);
+        const hiddenOutputs = hiddenInputs.map(this.sigmoid);
+        const finalInputs = this.feedForward(hiddenOutputs, this.weightsHiddenOutput);
+        return finalInputs.map(this.sigmoid);
+    }
+    private transposeMatrix(matrix: number[][]): number[][] {
+        return matrix[0].map((_, i) => matrix.map(row => row[i]));
+    }
+
+    private feedForward(inputs: number[], weights: number[][]): number[] {
+        const transposeWeights = this.transposeMatrix(weights);
+        return transposeWeights.map((row, i) => inputs.reduce((sum, input, j) => sum + input * row[j], 0));
+    }
+}
